2df0b1cc53c045c2fa6fa181e07e81c101e1d08b
Koboleh <koboleh712@gmail.com>
2024-04-23 11:03:10 +0300

    Created userController with needed endpoints

---
+++ UrlShortener/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using UrlShortener.DTOs.Request;
+using UrlShortener.Interfaces;
+
+namespace UrlShortener.Controllers;
+
+[ApiController]
+[Route("/api/user")]
+public class UserController : ControllerBase
+{
+    private readonly IUserService _userService;
+
+    public UserController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [AllowAnonymous]
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(LoginRequest request)
+    {
+
+        var result = await _userService.UserLogin(request);
+
+        if (result != null)
+        {
+            return Ok(result);
+        }
+        
+        return BadRequest("Invalid login or password");
+    }
+}
