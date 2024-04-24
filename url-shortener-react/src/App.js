4ad87dde83f5db288b9150865e7d5fb5e8b93dcf
Koboleh <koboleh712@gmail.com>
2024-04-24 11:47:56 +0300

    Added paths for pages

--- url-shortener-react/src/App.js
+++ url-shortener-react/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import LoginPage from "./pages/LoginPage/LoginPage";
+import UrlsListPage from "./pages/UrlsListPage/UrlsListPage";
+import UrlPage from "./pages/UrlPage/UrlPage";
 
 function App() {
 
   const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <UrlsListPage />,
+    },
+    {
+      path: "/urls/:id",
+      element: <UrlPage />,
+    },
     {
       path: "/login",
       element: <LoginPage />,
